9a90d92317c1ba289740d4ee5e288204c80fddae
yanj414 <56147990+yanj414@users.noreply.github.com>
2025-02-02 15:05:20 +1100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,25 @@
+function calculate() {
+  const num1 = parseFloat(document.getElementById('num1').value);
+  const num2 = parseFloat(document.getElementById('num2').value);
+  const operation = document.getElementById('operation').value;
+  let result;
+
+  switch (operation) {
+    case 'add':
+      result = num1 + num2;
+      break;
+    case 'subtract':
+      result = num1 - num2;
+      break;
+    case 'multiply':
+      result = num1 * num2;
+      break;
+    case 'divide':
+      result = num1 / num2;
+      break;
+    default:
+      result = 'Invalid operation';
+  }
+
+  document.getElementById('result').innerText = `Result: ${result}`;
+}
